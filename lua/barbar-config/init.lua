724f4c801ad39dfa8a3adbf351083e7edacfe9ee
Joan Pont <joan.pont19@gmail.com>
2022-04-09 11:49:29 +0200

    initial commit

---
+++ lua/barbar-config/init.lua
@@ -0,0 +1,5 @@
+vim.g.bufferline = {
+    animation = true,
+    closable = true,
+    clickable = true,
+}
---
+++ lua/packer-config/init.lua
@@ -0,0 +1,26 @@
+return require("packer").startup(function(use)
+    use 'wbthomason/packer.nvim'
+    use 'Mofiqul/dracula.nvim'
+    use 'kyazdani42/nvim-tree.lua'
+    use 'kyazdani42/nvim-web-devicons'
+    use 'nvim-lualine/lualine.nvim'
+    use 'romgrk/barbar.nvim'
+    use 'numToStr/Comment.nvim'
+    use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+    use 'nvim-treesitter/nvim-treesitter-refactor'
+    use 'JoosepAlviste/nvim-ts-context-commentstring'
+    use { 'nvim-telescope/telescope.nvim', requires = { {'nvim-lua/plenary.nvim'} } }
+    use 'lewis6991/gitsigns.nvim'
+
+
+-- LSP Configs
+    use 'neovim/nvim-lspconfig' -- Collection of configurations for the built-in LSP client
+    use 'williamboman/nvim-lsp-installer'
+    use 'hrsh7th/nvim-cmp' -- Autocompletion plugin
+    use 'hrsh7th/cmp-nvim-lsp' -- LSP source for nvim-cmp
+    use 'saadparwaiz1/cmp_luasnip' -- Snippets source for nvim-cmp
+    use 'L3MON4D3/LuaSnip' -- Snippets plugin
+    use 'jose-elias-alvarez/null-ls.nvim'
+    use 'onsails/lspkind-nvim'
+    use "rafamadriz/friendly-snippets"
+end)
