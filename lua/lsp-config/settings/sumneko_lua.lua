b6ecd8e9005d79be24694f46ae3bfddca1793d0c
Joan Pont <joan.pont19@gmail.com>
2022-06-13 23:57:27 +0200

    sunmeko lua changed defaults

--- lua/lsp-config/settings/sumneko_lua.lua
+++ lua/lsp-config/settings/sumneko_lua.lua
@@ -1,24 +1,24 @@
--- return {
--- 	settings = {
--- 		Lua = {
---       runtime = {
---         -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
---         -- version = "LuaJIT",
---         -- Setup your lua path
---         -- path = vim.split(package.path, ";"),
---       },
--- 			diagnostics = {
---         -- Get the language server to recognize the `vim` global
--- 				globals = { "vim" },
--- 			},
--- 			workspace = {
--- 				library = {
--- 					-- [vim.fn.expand("$VIMRUNTIME/lua")] = true,
--- 					-- [vim.fn.stdpath("config") .. "/lua"] = true,
--- 				},
--- 			},
--- 		},
--- 	},
--- }
-local status_ok, luadev pcall(require,"luadev")
-return luadev.setup({})
+return {
+	settings = {
+		Lua = {
+      runtime = {
+        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+        -- version = "LuaJIT",
+        -- Setup your lua path
+        -- path = vim.split(package.path, ";"),
+      },
+			diagnostics = {
+        -- Get the language server to recognize the `vim` global
+				globals = { "vim" },
+			},
+			workspace = {
+				library = {
+					-- [vim.fn.expand("$VIMRUNTIME/lua")] = true,
+					-- [vim.fn.stdpath("config") .. "/lua"] = true,
+				},
+			},
+		},
+	},
+}
+-- local status_ok, luadev pcall(require,"luadev")
+-- return luadev.setup({})
